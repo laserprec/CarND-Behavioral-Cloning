af7ced5ba8b8442dc20d8a8d31a658605fc39c1f
Jianjie Liu <liujianji3@gmail.com>
2018-06-09 22:16:25 +0000

    Add angle frequency report

--- behaviorClone.py
+++ behaviorClone.py
@@ -12,9 +12,9 @@ CSV_FILENAME    = '/driving_log.csv'
 
 ACTIVATION_FUNC = 'tanh'
 LOSS_FUNC       = 'mse'
-LEARNING_RATE   = 1e-4
-BATCH_SIZE      = 128
-NUM_EPOCH       = 3
+LEARNING_RATE   = 1e-3
+BATCH_SIZE      = 512
+NUM_EPOCH       = 6
 
 # Resolve plt backend issues in VM
 plt.switch_backend('agg')
